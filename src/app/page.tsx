4d0f693512542ca0906d6d6822e766472bcca232
enzo <enzogabrielcarvalho123@gmail.com>
2025-07-09 16:42:29 -0300

    Configurando Input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <h1> Hello world</h1>
-      <Button />
+      <Button type="submit">
+        Enviar
+        <ArrowRight />
+
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5"/>
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,14 @@
-interface ButtonProps {
-  text?: string
-}
+import { ComponentProps, ReactNode } from "react"
+
+interface ButtonProps extends ComponentProps<'button'> {}
 
 export function Button(props: ButtonProps) {
     return (
-    <button className="px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors hover:bg-blue hover:text-gray-900 duration-300">
-      {props.text || 'set-text'}
+    <button 
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors hover:bg-blue hover:text-gray-900 duration-300"
+      {...props}
+    >
+      {props.children}
     </button>
     )
 }
